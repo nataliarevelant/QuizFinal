1f8161b00b34510554019252e962f9b6312cfa0a
Natalia Revelant <natalia.revelant@gmail.com>
2022-05-08 20:32:26 -0300

    commit 4

--- app-02.js
+++ app-02.js
@@ -19,94 +19,154 @@ let correctAnswers = 0
 let totalAnswers = document.querySelector('.total')
 totalAnswers.textContent = quizzes.length
 
-//quizOne
+// quizOne
+
+//Adicionei duas classes iniciais para que o 'quizTwo' e o 'quizThree' permaneçam, por enquanto, inativos
+quizTwo.classList.add('inactive','opacity')
+quizThree.classList.add('inactive','opacity')
+
+// Cada elemento dentro da variável 'alternativesQuizOne' ouvirá um click, despertando o evento 'CheckAnswerone'.
 for ( let alternativeQuizOne of alternativesQuizOne ) {
     alternativeQuizOne.addEventListener( 'click', checkAnswerOne )
 }
 
-quizTwo.classList.add('inactive','opacity')
-quizThree.classList.add('inactive','opacity')
-
+// Criei evento para checar se a resposta do 'quizOne' está certa
 function checkAnswerOne ( event ) {
 
+    // Defini o target do evento. Isto é, as alternativas do 'quizOne'
     let alternativeQuizOne = event.target
 
+    // Adicionei uma classe 'clicked' com o intuito de mudar o estilo da alternativa clicada
     alternativeQuizOne.classList.add('clicked')
 
     if ( alternativeQuizOne.classList.contains('correct') ) {
 
+        // Uma vez que a alternativa clicada é a correta:
+        
+        // O status de andamento do PL passa a ser visível
         statusOne.style.display = 'initial'
+
+        // Adiciona uma resposta correta a contagem
         correctAnswers = correctAnswers + 1
+
+        // Mudar o texto para 'Acertou!'
         document.querySelector('.statusOne > .status-de-acerto').textContent = 'Acertou!'
 
     } else {
 
+        // Uma vez que a alternativa clicada é a incorreta:
+
+        // O status de andamento do PL passa a ser visível de qualquer forma
         statusOne.style.display = 'initial'
+
+        // Mudar o texto para 'Errou!'
         document.querySelector('.statusOne > .status-de-acerto').textContent = 'Errou!'
 
     }
 
+    // Uma vez já respondido, deixei o 'quizOne' inativo
     quizOne.classList.add('inactive')
+
+    // Removi as classes que inativam e diminuem a opacidade do 'quizTwo'
     quizTwo.classList.remove('inactive','opacity')
 
 }
 
 
 // quizTwo
+
+// Cada elemento dentro da variável 'alternativesQuizTwo' ouvirá um click, despertando o evento 'CheckAnswerone'.
 for ( let alternativeQuizTwo of alternativesQuizTwo ) {
     alternativeQuizTwo.addEventListener( 'click', checkAnswerTwo )
 }
 
+// Criei evento para checar se a resposta do 'quizTwo' está certa
 function checkAnswerTwo ( event ) {
 
+    // Defini o target do evento. Isto é, as alternativas do 'quizTwo'
     let alternativeQuizTwo = event.target
 
+    // Adicionei uma classe 'clicked' com o intuito de mudar o estilo da alternativa clicada
     alternativeQuizTwo.classList.add('clicked')
 
     if ( alternativeQuizTwo.classList.contains('correct') ) {
 
+        // Uma vez que a alternativa clicada é a correta:
+
+        // O status de andamento do respectivo PL passa a ser visível
         statusTwo.style.display = 'initial'
+
+        // Adiciona uma resposta correta a contagem
         correctAnswers = correctAnswers + 1
+
+        // Mudar o texto para 'Acertou!'
         document.querySelector('.statusTwo > .status-de-acerto').textContent = 'Acertou!'
 
     } else {
 
+        // Uma vez que a alternativa clicada é a incorreta:
+
+        // O status de andamento do PL passa a ser visível de qualquer forma
         statusTwo.style.display = 'initial'
+
+        // Mudar o texto para 'Errou!'
         document.querySelector('.statusTwo > .status-de-acerto').textContent = 'Errou!'
 
     }
 
+    // Uma vez já respondido, deixei o 'quizTwo' inativo, assim como o 'quizOne
     quizTwo.classList.add('inactive')
+
+    // Removi as classes que inativam e diminuem a opacidade do 'quizThree'
     quizThree.classList.remove('inactive','opacity')
 
 }
 
 // quizThree
+
+// Cada elemento dentro da variável 'alternativesQuizThree' ouvirá um click, despertando o evento 'CheckAnswerone'.
 for ( let alternativeQuizThree of alternativesQuizThree ) {
     alternativeQuizThree.addEventListener( 'click', checkAnswerThree )
 }
 
+// Criei evento para checar se a resposta do 'quizThree' está certa
 function checkAnswerThree ( event ) {
 
+    // Defini o target do evento. Isto é, as alternativas do 'quizThree'
     let alternativeQuizThree = event.target
 
+    // Adicionei uma classe 'clicked' com o intuito de mudar o estilo da alternativa clicada
     alternativeQuizThree.classList.add('clicked')
 
     if ( alternativeQuizThree.classList.contains('correct') ) {
 
+        // Uma vez que a alternativa clicada é a correta:
+
+        // O status de andamento do respectivo PL passa a ser visível
         statusThree.style.display = 'initial'
+
+        // Adiciona uma resposta correta a contagem
         correctAnswers = correctAnswers + 1
+
+        // Mudar o texto para 'Acertou!'
         document.querySelector('.statusThree > .status-de-acerto').textContent = 'Acertou!'
 
     } else {
 
+        // Uma vez que a alternativa clicada é a incorreta:
+
+        // O status de andamento do PL passa a ser visível de qualquer forma
         statusThree.style.display = 'initial'
+
+        // Mudar o texto para 'Errou!'
         document.querySelector('.statusThree > .status-de-acerto').textContent = 'Errou!'
 
     }
 
+    // Uma vez já respondido, deixei o 'quizOne' inativo
     quizThree.classList.add('inactive')
 
+    // Selecionei o elemento HTML 'output' e substitui o seu respectivo texto pelo número total de acertos.
     document.querySelector( 'output' ).textContent = correctAnswers
 
 }
